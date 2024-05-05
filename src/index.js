382a0f0f6575ba1a2873931de0011952b0a4f3ac
bima-taruna <bimataruna21@gmail.com>
2024-05-05 15:32:11 +0700

    styling header and nav

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./style.css";
+import Logo from "./images/logo1.png";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 
-console.log("succes");
+function Header() {
+  const header = document.querySelector("header");
+
+  const myLogo = new Image();
+  myLogo.src = Logo;
+
+  header.appendChild(myLogo);
+
+  return header;
+}
+
+document.body.appendChild(Header());
