d9637f5020f0ec0d63833782da6e18f11b714d62
bima-taruna <bimataruna21@gmail.com>
2024-05-07 16:03:39 +0700

    styling the home page

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ export default function home() {
   homeSection.innerHTML = `
     <div class="left-side">
       <div class="headline">Discover The Art of Perfect Coffee</div>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, unde recusandae, blanditiis magnam maiores accusantium ducimus voluptatem in sit voluptatum libero. Et repellendus id omnis rem veritatis laboriosam ratione obcaecati!</p>
+      <p class="home-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, unde recusandae, blanditiis magnam maiores accusantium ducimus voluptatem in sit voluptatum libero. Et repellendus id omnis rem veritatis laboriosam ratione obcaecati!</p>
       <div class="home-buttons">
           <button class="order-button">Order Now</button>
           <button class="explore-button">Explore More</button>
