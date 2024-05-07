27112de85d555815101aab946809447c2f4876c3
bima-taruna <bimataruna21@gmail.com>
2024-05-07 15:51:14 +0700

    populate home.js with some html

--- src/home.js
+++ src/home.js
@@ -1,7 +1,18 @@
 export default function home() {
   const homeSection = document.createElement("section");
   homeSection.classList.add("home");
-  homeSection.innerHTML = "HOME";
+
+  homeSection.innerHTML = `
+    <div class="left-side">
+      <div class="headline">Discover The Art of Perfect Coffee</div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, unde recusandae, blanditiis magnam maiores accusantium ducimus voluptatem in sit voluptatum libero. Et repellendus id omnis rem veritatis laboriosam ratione obcaecati!</p>
+      <div class="home-buttons">
+          <button class="order-button">Order Now</button>
+          <button class="explore-button">Explore More</button>
+      </div>
+    </div>
+    <div class="right-side"></div>
+  `;
 
   return homeSection;
 }
