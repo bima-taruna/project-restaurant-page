97e9b49449fcfa3a28c4d4ef4c0030ada2af98c1
bima-taruna <bimataruna21@gmail.com>
2024-05-09 19:50:12 +0700

    add more menu data

--- src/data/menu-data.js
+++ src/data/menu-data.js
@@ -1,6 +1,9 @@
 import Latte from "../images/latte.jpg";
 import Cappucino from "../images/cappucino.jpg";
 import Americano from "../images/americano.jpg";
+import Doppio from "../images/doppio.jpg";
+import Espresso from "../images/espresso.jpg";
+import Machiato from "../images/machiato.jpg";
 
 export const data = [
   {
@@ -14,5 +17,17 @@ export const data = [
   {
     image: Americano,
     name: "Americano",
+  },
+  {
+    image: Doppio,
+    name: "Doppio",
+  },
+  {
+    image: Machiato,
+    name: "Machiato",
+  },
+  {
+    image: Espresso,
+    name: "Espresso",
   },
 ];
